1bec1b2ec74e0d7967623084f5a694c646da6fd5
szu-advtech <none>
2023-03-14 17:16:58 +0800

    Initial commit

---
+++ change/Examples_old/ROS/ORB_SLAM3/src/ros_mono_inertial.cc
@@ -0,0 +1,202 @@
+/**
+* This file is part of ORB-SLAM3
+*
+* Copyright (C) 2017-2021 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+*
+* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+#include<vector>
+#include<queue>
+#include<thread>
+#include<mutex>
+
+#include<ros/ros.h>
+#include<cv_bridge/cv_bridge.h>
+#include<sensor_msgs/Imu.h>
+
+#include<opencv2/core/core.hpp>
+
+#include"../../../include/System.h"
+#include"../include/ImuTypes.h"
+
+using namespace std;
+
+/*IMU获取*/
+class ImuGrabber
+{
+public:
+    ImuGrabber(){};
+    void GrabImu(const sensor_msgs::ImuConstPtr &imu_msg);
+
+    queue<sensor_msgs::ImuConstPtr> imuBuf;
+    std::mutex mBufMutex;//IMU和图像的互斥锁？
+};
+
+/*原始图像获取、图像矩阵获取、与IMU同步*/
+class ImageGrabber
+{
+public:
+    ImageGrabber(ORB_SLAM3::System* pSLAM, ImuGrabber *pImuGb, const bool bClahe): mpSLAM(pSLAM), mpImuGb(pImuGb), mbClahe(bClahe){}
+
+    void GrabImage(const sensor_msgs::ImageConstPtr& msg);
+    cv::Mat GetImage(const sensor_msgs::ImageConstPtr &img_msg);
+    void SyncWithImu();
+
+    queue<sensor_msgs::ImageConstPtr> img0Buf;
+    std::mutex mBufMutex;//IMU和图像的互斥锁？
+   
+    ORB_SLAM3::System* mpSLAM;
+    ImuGrabber *mpImuGb;
+
+    const bool mbClahe;
+    cv::Ptr<cv::CLAHE> mClahe = cv::createCLAHE(3.0, cv::Size(8, 8));
+};
+
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "Mono_Inertial");//初始化节点mono_Inertial
+  ros::NodeHandle n("~");//指定命名空间
+  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);//输出info级别的调试信息
+  bool bEqual = false;//是否需要图像矫正
+  if(argc < 3 || argc > 4)
+  {
+    cerr << endl << "Usage: rosrun ORB_SLAM3 Mono_Inertial path_to_vocabulary path_to_settings [do_equalize]" << endl;
+    ros::shutdown();
+    return 1;
+  }//输入参数的个数不等于3或4则shutdown
+
+  /*判断是否需要图像矫正*/
+  if(argc==4)
+  {
+    std::string sbEqual(argv[3]);
+    if(sbEqual == "true")
+      bEqual = true;
+  }
+
+  // Create SLAM system. It initializes all system threads and gets ready to process frames.
+  ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::IMU_MONOCULAR,true);//创建slam,初始化所有线程并准备处理帧
+
+  ImuGrabber imugb;
+  ImageGrabber igb(&SLAM,&imugb,bEqual); // TODO
+  
+  // Maximum delay, 5 seconds
+  ros::Subscriber sub_imu = n.subscribe("/imu", 1000, &ImuGrabber::GrabImu, &imugb); 
+  ros::Subscriber sub_img0 = n.subscribe("/camera/image_raw", 100, &ImageGrabber::GrabImage,&igb);
+
+  std::thread sync_thread(&ImageGrabber::SyncWithImu,&igb);//开启一个线程同步IMU和图像数据
+
+  ros::spin();
+
+  return 0;
+}
+
+/*获取图像放入img_msg，这个过程和imu获取过程互斥，要用互斥锁*/
+void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr &img_msg)
+{
+  mBufMutex.lock();//锁上
+  if (!img0Buf.empty())
+    img0Buf.pop();
+  img0Buf.push(img_msg);
+  mBufMutex.unlock();//开锁
+}
+
+/*将原始图像转化为mat格式*/
+cv::Mat ImageGrabber::GetImage(const sensor_msgs::ImageConstPtr &img_msg)
+{
+  // Copy the ros image message to cv::Mat.
+  cv_bridge::CvImageConstPtr cv_ptr;
+  try
+  {
+    cv_ptr = cv_bridge::toCvShare(img_msg, sensor_msgs::image_encodings::MONO8);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+  }
+  
+  if(cv_ptr->image.type()==0)
+  {
+    return cv_ptr->image.clone();
+  }
+  else
+  {
+    std::cout << "Error type" << std::endl;
+    return cv_ptr->image.clone();
+  }
+}
+
+
+void ImageGrabber::SyncWithImu()
+{
+  while(1)
+  {
+    cv::Mat im;
+    double tIm = 0;
+    if (!img0Buf.empty()&&!mpImuGb->imuBuf.empty())
+    {
+      tIm = img0Buf.front()->header.stamp.toSec();//图像时间戳
+      if(tIm>mpImuGb->imuBuf.back()->header.stamp.toSec()) 
+          //如果图像时间比最新的IMU数据大，说明IMU数据还没准备好，需要等一下IMU数据
+          continue;
+      {
+      this->mBufMutex.lock();
+      im = GetImage(img0Buf.front());//获取图像，ROS格式转成opencv格式
+      img0Buf.pop();
+      this->mBufMutex.unlock();
+      }
+
+      vector<ORB_SLAM3::IMU::Point> vImuMeas;
+      mpImuGb->mBufMutex.lock();
+      if(!mpImuGb->imuBuf.empty())
+      {
+        // Load imu measurements from buffer
+        vImuMeas.clear();
+        //获取当前图像时间戳之前的imu数据
+        while(!mpImuGb->imuBuf.empty() && mpImuGb->imuBuf.front()->header.stamp.toSec()<=tIm)
+        {
+          double t = mpImuGb->imuBuf.front()->header.stamp.toSec();
+          cv::Point3f acc(mpImuGb->imuBuf.front()->linear_acceleration.x, mpImuGb->imuBuf.front()->linear_acceleration.y, mpImuGb->imuBuf.front()->linear_acceleration.z);
+          cv::Point3f gyr(mpImuGb->imuBuf.front()->angular_velocity.x, mpImuGb->imuBuf.front()->angular_velocity.y, mpImuGb->imuBuf.front()->angular_velocity.z);
+          vImuMeas.push_back(ORB_SLAM3::IMU::Point(acc,gyr,t));
+          mpImuGb->imuBuf.pop();
+        }
+      }
+      mpImuGb->mBufMutex.unlock();
+      //图像直方图均衡，mbClache在ImageGrabber构造函数中设置
+      if(mbClahe)
+        mClahe->apply(im,im);
+      //开始跟踪
+      mpSLAM->TrackMonocular(im,tIm,vImuMeas);
+    }
+
+    std::chrono::milliseconds tSleep(1);
+    std::this_thread::sleep_for(tSleep);
+  }
+}
+
+void ImuGrabber::GrabImu(const sensor_msgs::ImuConstPtr &imu_msg)
+{
+  mBufMutex.lock();
+  imuBuf.push(imu_msg);
+  mBufMutex.unlock();
+  return;
+}
+
+
